fffbb7f8b09398cff096ee3dbbf7d73c48284725
Hudson Burke <hpb7kr@virginia.edu>
2022-06-17 18:24:54 -0400

    Working with multiple homes. TODO: Find a way to send joint home command

--- src/arm_class.py
+++ src/arm_class.py
@@ -1,3 +1,5 @@
+# Author: Hudson Burke
+
 import threading
 import rospy
 from relaxed_ik_ros1.msg import EEPoseGoals
@@ -8,7 +10,6 @@ from geometry_msgs.msg import Pose, TransformStamped
 import transformations as T
 from tf_functions import pose_lookup, transform
 import numpy as np
-from se3_conversion import msg_to_se3
 from tf2_ros import StaticTransformBroadcaster
 from math import pi
 
@@ -21,8 +22,8 @@ class Arm:
         self.angular_pose_pub = rospy.Publisher(
             "ur/ur_arm_scaled_pos_joint_traj_controller/command", JointTrajectory, queue_size=10)
         self.joint_states_sub = rospy.Subscriber(
-            "ur_joint_states", JointState, self.js_cb)  # TODO: remap joint_states to ur_joint_states
-        self.rleaxed_ik_joint_angles = rospy.Subscriber(
+            "ur/joint_states", JointState, self.js_cb)
+        self.relaxed_ik_joint_angles = rospy.Subscriber(
             "/relaxed_ik/joint_angle_solutions", JointAngles, self.rik_ja_cb)
 
         self.joint_states = np.zeros(6)
@@ -32,17 +33,23 @@ class Arm:
         self.seq = 0
         self.home = home
 
+        
         # UR Interface
         rospy.loginfo("Initializing UR Interface...")
-        rospy.sleep(2)
+        rospy.sleep(1)
         rospy.loginfo("Homing...")
-        self.send_to_home()
+        self.initial_home()
         # wait until reaches home
-        rospy.sleep(8)
+        rospy.sleep(10)
         rospy.loginfo("Sending transforms")
         self.send_transforms()
-        rospy.sleep(2)
+        rospy.sleep(1)
+        # clear the relaxed_ik commands
+        # self.clear_goal()
+        self.send_to_home()
+        rospy.sleep(1)
         rospy.loginfo("Ready to begin tasks")
+
 
         thread_loop = threading.Thread(target=self.joint_command_loop)
         thread_loop.start()
@@ -51,10 +58,9 @@ class Arm:
         joint_angles = np.array(data.angles.data)
         self.joint_command[0:3] = np.flip(joint_angles)[3:]
         self.joint_command[3:] = joint_angles[3:]
-        # self.send_joint_command(*self.joint_command.tolist())
+        
 
     def js_cb(self, data):
-        time = rospy.Time.now()
         self.joint_states = np.array(data.position)
 
     def joint_command_loop(self):
@@ -63,38 +69,36 @@ class Arm:
             self.send_joint_command(*self.joint_command.tolist())
             rate.sleep()
 
-    # TODO: choose between global and local frames
     def send_goal(self, x, y, z, roll=0, pitch=0, yaw=0, frame="ur_arm_ee_link"):
         t_desired = np.array([[x],
                              [y],
                              [z]])
-        # np.array([[-1, 0,0],[0,-1,0],[0,0,1]])
-        R_desired = T.euler_matrix(roll, pitch, yaw)[:3, :3]  # np.eye(3)
+        if roll != 0 or pitch != 0 or yaw != 0:
+            R_desired = T.euler_matrix(roll, pitch, yaw)[:3, :3]
+        else:
+            R_desired = np.eye(3)
         desired = np.block([[R_desired, t_desired], [0, 0, 0, 1]])
-        rospy.loginfo("Desired State: \n" + str(desired))
-        # TODO: find the problem here
-        state_transform = pose_lookup("ur_arm_starting_pose", frame)
-        rospy.loginfo(str(state_transform))
-        state = msg_to_se3(state_transform)
 
-        rospy.loginfo("Current State: \n" + str(state))
+        state_transform = pose_lookup("ur_arm_starting_pose", frame)
+        state = transform_stamped_to_se3(state_transform)
 
         final_state = np.matmul(state, desired)
         t_final = final_state[:, 3][:3]
         R_final = final_state[:3, :3]
 
-        q = T.quaternion_from_matrix(R_final)
+        q = T.quaternion_from_matrix(final_state)
+        # q = T.quaternion_from_matrix(R_final)
 
-        rospy.loginfo("Final State: \n" + str(final_state))
+
         ee_pose = Pose()
         ee_pose_goal = EEPoseGoals()
         ee_pose.position.x = float(t_final[0])
         ee_pose.position.y = float(t_final[1])
         ee_pose.position.z = float(t_final[2])
-        ee_pose.orientation.w = float(q[3])
-        ee_pose.orientation.x = float(q[0])
-        ee_pose.orientation.y = float(q[1])
-        ee_pose.orientation.z = float(q[2])
+        ee_pose.orientation.w = float(q[0])
+        ee_pose.orientation.x = float(q[1])
+        ee_pose.orientation.y = float(q[2])
+        ee_pose.orientation.z = float(q[3])
 
         ee_pose_goal.ee_poses.append(ee_pose)
         ee_pose_goal.header.seq = self.seq
@@ -125,32 +129,51 @@ class Arm:
         try:
             error = np.abs(self.joint_command - self.joint_states)
         except:
-            error = np.ones(6)
-        w = np.array([2, 2, 2, 0.2, 0.2, 0.2])  # TODO: ask about these weights
-        # TODO: maybe should be multiply
+            error = 5*np.ones(6)
+        w = np.array([3, 3, 3, 0.2, 0.2, 0.2])
+        # TODO: maybe should be matmul
         weight = np.abs(np.sum(np.multiply(w, error)))
-        point.time_from_start = rospy.Duration(max(weight, 0.5))
-        # rospy.set_param('/scaled_vel_joint_traj_controller/constraints/goal_time',5*int(weight))
+        point.time_from_start = rospy.Duration(max(weight, 1.0))
 
         self.angular_pose_pub.publish(msg)
 
     def send_to_home(self):
         self.send_joint_command(*self.home)
 
+    def initial_home(self):
+        self.send_joint_command(*self.home)
+
+    def clear_goal(self):
+        blank = EEPoseGoals()
+        blank_pose = Pose()
+        blank.ee_poses.append(blank_pose)
+        blank.header.seq = self.seq
+        self.ee_pose_goals_pub.publish(blank) # punch relaxed_ik
+        self.seq += 1
+    
     def send_transforms(self):
         broadcaster = StaticTransformBroadcaster()
         ee_link_pose = TransformStamped()
-        ee_link = transform(ee_link_pose, "ur_arm_flange",
-                            "ur_arm_ee_link", -pi, 0, 0)
-
         flange_to_base = pose_lookup("ur_arm_base_link", "ur_arm_flange")
         if flange_to_base is not None:
+            rot = flange_to_base.transform.rotation
+            ee_link = transform(ee_link_pose, "ur_arm_flange",
+                            "ur_arm_ee_link", rot.x, rot.y, rot.z, rot.w)
             starting_pose = transform(
                 flange_to_base, "ur_arm_base_link", "ur_arm_starting_pose", 0, 0, 0)
         else:
             starting_pose = None
-            rospy.logwarn(
+            rospy.logerr(
                 "No transform found from ur_arm _flange to ur_arm_base_link.")
 
         if ee_link is not None and starting_pose is not None:
             broadcaster.sendTransform([ee_link, starting_pose])
+
+def transform_stamped_to_se3(msg : TransformStamped):
+    pose = msg.transform.translation
+    rot = msg.transform.rotation
+    t = np.array([[pose.x], [pose.y], [pose.z]])
+    rot_matrix = T.quaternion_matrix([rot.w, rot.x, rot.y, rot.z])[:3, :3]
+    se3 = np.block([[rot_matrix, t], [0, 0, 0, 1]])
+    return se3
+
--- src/tf_functions.py
+++ src/tf_functions.py
@@ -17,7 +17,26 @@ def pose_lookup(parent, child):
         rospy.sleep(5)
 
 
-def transform(pose, parent, child, x, y, z):
+# def transform(parent, child, x, y, z, roll, pitch, yaw):
+#     static_transformStamped = TransformStamped()
+
+#     static_transformStamped.header.stamp = rospy.Time.now()
+#     static_transformStamped.header.frame_id = parent
+#     static_transformStamped.child_frame_id = child
+
+#     static_transformStamped.transform.translation.x = float(x)
+#     static_transformStamped.transform.translation.y = float(y)
+#     static_transformStamped.transform.translation.z = float(z)
+
+#     quat = quaternion_from_euler(float(roll), float(pitch), float(yaw))
+#     static_transformStamped.transform.rotation.x = quat[0]
+#     static_transformStamped.transform.rotation.y = quat[1]
+#     static_transformStamped.transform.rotation.z = quat[2]
+#     static_transformStamped.transform.rotation.w = quat[3]
+
+#     return static_transformStamped
+
+def transform(pose, parent, child, x, y, z, w=None):
     static_transformStamped = TransformStamped()
 
     static_transformStamped.header.stamp = rospy.Time.now()
@@ -30,11 +49,16 @@ def transform(pose, parent, child, x, y, z):
         pose.transform.translation.y)
     static_transformStamped.transform.translation.z = float(
         pose.transform.translation.z)
-
-    quat = quaternion_from_euler(float(x), float(y), float(z))
-    static_transformStamped.transform.rotation.x = quat[0]
-    static_transformStamped.transform.rotation.y = quat[1]
-    static_transformStamped.transform.rotation.z = quat[2]
-    static_transformStamped.transform.rotation.w = quat[3]
+    if w is not None:
+        static_transformStamped.transform.rotation.x = x
+        static_transformStamped.transform.rotation.y = y
+        static_transformStamped.transform.rotation.z = z
+        static_transformStamped.transform.rotation.w = w
+    else:
+        quat = quaternion_from_euler(float(x), float(y), float(z))
+        static_transformStamped.transform.rotation.x = quat[0]
+        static_transformStamped.transform.rotation.y = quat[1]
+        static_transformStamped.transform.rotation.z = quat[2]
+        static_transformStamped.transform.rotation.w = quat[3]
 
     return static_transformStamped
