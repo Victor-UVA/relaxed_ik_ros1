6e9fe381805fb808bdb1161f067e7d63ef109111
Hudson Burke <hpb7kr@virginia.edu>
2022-06-22 17:07:20 -0400

    updated rps

--- src/rps.py
+++ src/rps.py
@@ -1,84 +1,85 @@
 #!/usr/bin/python3
 
 #### ASSUMES ARM FACING FRONT OF HUSKY AND STARTING FROM BOT LEFT CORNER ####
-from sympy import capture, true
 import rospy
 from arm_class import Arm
 from std_msgs.msg import Bool, Float64
-import threading
-import multiprocessing
-import pyflycap2
-
-# Tunable constants
-x_stride = 0.05  # m
-y_stride = 0.05  # m
-z_stride = 0.05  # m
-desired_distance = 0.15  # m
-tolerance = 0.001  # m
-fovZ = 9.15  # cm
-fovY = 15.5  # cm
-scanHeight = 20  # ?
-scanWidth = 50  # ?
-heightSteps = scanHeight / (fovY - 1)
-widthSteps = scanWidth / (fovZ - 1)
-
-# Flags
-correct_distance = False
-
-# Initialize global arm
-# may need to go in main, not exactly sure how python global works
-arm = Arm(config_file_name="ur5e_info.yaml")
+from pyflycap2.interface import Camera
 
 
-def prox_cb(data: Float64):
-    global correct_distance, arm
-    distance_m = data.data/100  # converting from centimeters to meters
-    if distance_m > desired_distance-tolerance:
-        arm.send_goal(0.0, y_stride, 0.0)
-    elif distance_m < desired_distance+tolerance:
-        arm.send_goal(0.0, -y_stride, 0.0)
-    else:
-        correct_distance = True
-        return
-    correct_distance = False
+class RPS:
+    def __init__(self, file_prefix: str, arm_config_file_name: str):
+        # Tunable constants
+        self.x_stride = 0.05  # m
+        self.y_stride = 0.05  # m
+        self.z_stride = 0.05  # m
+        self.desired_distance = 0.15  # m
+        self.tolerance = 0.001  # m
+        self.fovZ = 9.15  # cm
+        self.fovY = 15.5  # cm
+        self.scanHeight = 20  # ?
+        self.scanWidth = 50  # ?
+        self.heightSteps = self.scanHeight / (self.fovY - 1)
+        self.widthSteps = self.scanWidth / (self.fovZ - 1)
+        self.file_prefix = file_prefix
+        # Flags
+        self.correct_distance = False
+        # Initialize arm
+        self.arm = Arm(config_file_name=arm_config_file_name)
+        # Topic stuff
+        self.arm_distance_sub = rospy.Subscriber(
+            '/rps/arm_distance', Float64, self.prox_cb)
+        self.take_pics_pub = rospy.Publisher(
+            '/rps/take_pics', Bool, queue_size=1)
+        # Initialize camera
+        self.cam = Camera()
+        self.cam.connect()
+        self.cam.set_drop_mode(drop=False)  # allow buffering
 
+    def prox_cb(self, data: Float64):
+        distance_m = data.data/100  # converting from centimeters to meters
+        if distance_m > self.desired_distance-self.tolerance:
+            self.arm.send_goal(0.0, self.y_stride, 0.0)
+        elif distance_m < self.desired_distance+self.tolerance:
+            self.arm.send_goal(0.0, -self.y_stride, 0.0)
+        else:
+            self.correct_distance = True
+            return
+        self.correct_distance = False
 
-def capture_pic():
-    take_pics_pub = rospy.Publisher('/rps/take_pics', Bool, queue_size=1)
-    take_pics_pub.publish(Bool(True))
-    rospy.wait_for_message("/rps/done", Bool)
-    take_pics_pub.publish(Bool(False))
-    # save pictures
+    def capture_pic(self):
+        num_pics = 8
+        self.take_pics_pub.publish(Bool(True))
+        rospy.wait_for_message("/rps/done", Bool)
+        self.take_pics_pub.publish(Bool(False))
+        # save pictures
+        for i in range(num_pics):
+            self.cam.read_next_image()
+            self.cam.save_current_image(self.file_prefix + "-" + str(i))
 
-
-def move_sequence():
-    global arm
-    for i in range(widthSteps/2):  # traverse width
-        capture_pic()
-        for j in range(heightSteps):  # move up
-            arm.send_goal(0.0, 0.0, z_stride)
-            rospy.sleep(1)
-            capture_pic()
-        arm.send_goal(x_stride, 0.0, 0.0)  # move to the right
-        capture_pic()
-        for k in range(heightSteps):  # move down
-            arm.send_goal(0.0, 0.0, -z_stride)
-            rospy.sleep(1)
-            capture_pic()
-        arm.send_goal(x_stride, 0.0, 0.0)  # move to the right
-        rate.sleep()
+    def move_sequence(self):
+        for i in range(self.widthSteps/2):  # traverse width
+            self.capture_pic()
+            for j in range(self.heightSteps):  # move up
+                self.arm.send_goal(0.0, 0.0, self.z_stride)
+                rospy.sleep(1)
+                self.capture_pic()
+            self.arm.send_goal(self.x_stride, 0.0, 0.0)  # move to the right
+            self.capture_pic()
+            for k in range(self.heightSteps):  # move down
+                self.arm.send_goal(0.0, 0.0, -self.z_stride)
+                rospy.sleep(1)
+                self.capture_pic()
+            self.arm.send_goal(self.x_stride, 0.0, 0.0)  # move to the right
+            rate.sleep()
 
 
 if __name__ == "__main__":
     rospy.init_node("rps_control")
-    arm_distance_sub = rospy.Subscriber('/rps/arm_distance', Float64, prox_cb)
     rate = rospy.Rate(5)
-    move_thread = threading.Thread(target=move_sequence)
-    lock = threading.Lock()
-    cond = threading.Condition()
-    cond.wait_for()
+    rps = RPS(arm_config_file_name="ur5e_info.yaml")
     while not rospy.is_shutdown():
-        if not correct_distance:
-            move_thread.start()
+        if rps.correct_distance:
+            rps.move_sequence()
             break
         rate.sleep()
